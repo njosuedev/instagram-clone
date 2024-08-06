1f47725f4ef353349c83994f04d728290b73b468
njosuedev <njosueofficial@gmail.com>
2024-08-06 11:45:58 +0200

    simplicity added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -104,7 +104,7 @@ const Home = () => {
                             </div>
                         </div>
                         <div className='postOneImage postFirt'>
-                        <video width="600" autoPlay>
+                        <video width="600" controls autoPlay muted>
                             <source src="./videos/video_1.mp4" type="video/mp4" />
                             Your browser does not support the video tag.
                         </video>
@@ -204,7 +204,7 @@ const Home = () => {
                     </div>
                     <div className="footerCopyRight">
                         <small>
-                            &copy; INSTAGRAM-CLONE BY N JOSUE
+                            &copy; INSTAGRAM-CLONE BY N JOSUE DEV
                         </small>
                     </div>
                 </div>
